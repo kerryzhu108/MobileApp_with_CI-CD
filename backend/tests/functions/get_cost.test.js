45e5f81967b89b3f27ed565c935c7e21058e955e
Zachary Vincze <zacharyvincze@gmai.com>
2021-10-02 16:23:38 -0400

    Added some additional unit tests

--- backend/tests/functions/get_cost.test.js
+++ backend/tests/functions/get_cost.test.js
@@ -7,3 +7,47 @@ test("gets cost of single known item", () => {
 test("throw an error with invalid item name", () => {
     expect(() => functions.get_cost("pineapple")).toThrowError();
 });
+
+test("throw an error with invalid item name in total cost", () => {
+    var checkout = [
+        {
+            "item": "pineapple",
+            "quantity": 1
+        }
+    ]
+    expect(() => functions.calculate_total_cost(checkout, 0.13)).toThrowError();
+});
+
+test("throw an error with invalid item quantity", () => {
+    var checkout = [
+        {
+            "item": "apple",
+            "quantity": -1
+        }
+    ]
+    expect(() => functions.calculate_total_cost(checkout, 0.13)).toThrowError();
+});
+
+test("calculate the total cost of a single item", () => {
+    var checkout = [
+        {
+            "item": "apple",
+            "quantity": 5
+        }
+    ]
+    expect(functions.calculate_total_cost(checkout, 0.13)).toBeCloseTo(5.65, 2);
+});
+
+test("calculate the total cost of multiple items", () => {
+    var checkout = [
+        {
+            "item": "apple",
+            "quantity": 5
+        },
+        {
+            "item": "banana",
+            "quantity": 2
+        }
+    ]
+    expect(functions.calculate_total_cost(checkout, 0.13)).toBeCloseTo(9.04, 2);
+});
